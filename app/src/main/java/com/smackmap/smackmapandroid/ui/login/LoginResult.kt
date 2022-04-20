07533307745308d13cd6adb35e02a0f1e54a39f0
Attila Palfi <attila.palfi.92@gmail.com>
2022-04-20 18:04:54 +0200

    initial commit

---
+++ app/src/main/java/com/smackmap/smackmapandroid/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.smackmap.smackmapandroid.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
