146ec3a031e88a413a7523c5c1ebea9edf58f078
Attila Palfi <attila.palfi.92@gmail.com>
2022-04-22 08:46:31 +0200

    basic tab layout 1 todo: migrate to tab layout 2

--- app/src/main/java/com/smackmap/smackmapandroid/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/smackmap/smackmapandroid/ui/main/SectionsPagerAdapter.kt
@@ -4,12 +4,6 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.smackmap.smackmapandroid.R
-
-private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
-)
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -24,12 +18,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
         return PlaceholderFragment.newInstance(position + 1)
     }
 
-    override fun getPageTitle(position: Int): CharSequence {
-        return context.resources.getString(TAB_TITLES[position])
-    }
-
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        return 4
     }
 }
