7744f85c5c6c419b1477ffe82d79f1743ddef558
Attila Palfi <attila.palfi.92@gmail.com>
2022-04-27 01:57:24 +0200

    profile view nice

--- app/src/main/java/com/smackmap/smackmapandroid/service/ServiceUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.smackmap.smackmapandroid.service
-
-import android.content.Context
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
-import com.smackmap.smackmapandroid.R
-
