0863f694c34f1a8f51e6a7a78a74e5543011edbb
Attila Palfi <attila.palfi.92@gmail.com>
2022-05-10 17:49:44 +0200

    nice datetimeformat

--- app/src/main/java/com/lovemap/lovemapandroid/ui/main/love/RecordLoveFragment.kt
+++ app/src/main/java/com/lovemap/lovemapandroid/ui/main/love/RecordLoveFragment.kt
@@ -1,13 +1,32 @@
 package com.lovemap.lovemapandroid.ui.main.love
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.lovemap.lovemapandroid.R
+import com.noowenz.customdatetimepicker.CustomDateTimePicker
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.*
 
-class RecordLoveFragment : Fragment() {
+class RecordLoveFragment : Fragment(), CustomDateTimePicker.ICustomDateTimeListener {
+
+    private val dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+
+    private lateinit var datePicker: CustomDateTimePicker
+    private lateinit var recordLoveHappenedAt: TextView
+    private lateinit var recordLoveChangeDateTime: Button
+
+    private var selectedTime: Instant = Instant.now()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +37,45 @@ class RecordLoveFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record_love, container, false)
+        val view = inflater.inflate(R.layout.fragment_record_love, container, false)
+        datePicker = CustomDateTimePicker(requireActivity(), this)
+        datePicker.set24HourFormat(true)
+
+        recordLoveChangeDateTime = view.findViewById(R.id.recordLoveChangeDateTime)
+        recordLoveHappenedAt = view.findViewById(R.id.recordLoveHappenedAt)
+
+        recordLoveHappenedAt.text = ZonedDateTime.now().format(dateTimeFormatter)
+        recordLoveChangeDateTime.setOnClickListener {
+            datePicker.setDate(Calendar.getInstance())
+            datePicker.showDialog()
+        }
+        return view
+    }
+
+    override fun onCancel() {
+
+    }
+
+    override fun onSet(
+        dialog: Dialog,
+        calendarSelected: Calendar,
+        dateSelected: Date,
+        year: Int,
+        monthFullName: String,
+        monthShortName: String,
+        monthNumber: Int,
+        day: Int,
+        weekDayFullName: String,
+        weekDayShortName: String,
+        hour24: Int,
+        hour12: Int,
+        min: Int,
+        sec: Int,
+        AM_PM: String
+    ) {
+        val timeZone = TimeZone.getDefault()
+        val zonedDateTime = ZonedDateTime.of(year, monthNumber, day, hour24, min, sec, 0, timeZone.toZoneId())
+        selectedTime = zonedDateTime.toInstant()
+        recordLoveHappenedAt.text = zonedDateTime.format(dateTimeFormatter)
     }
 }
