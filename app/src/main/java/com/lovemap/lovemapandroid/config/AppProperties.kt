9995e47be6b1489164934c4e8f336ed909a98a33
Attila Palfi <attila.palfi.92@gmail.com>
2022-05-29 21:15:42 +0200

    reconfiguring to prod api

--- app/src/main/java/com/lovemap/lovemapandroid/config/AppProperties.kt
+++ app/src/main/java/com/lovemap/lovemapandroid/config/AppProperties.kt
@@ -2,8 +2,8 @@ package com.lovemap.lovemapandroid.config
 
 const val AUTHORIZATION_HEADER = "Authorization"
 
-const val API_ENDPOINT = "http://192.168.0.58:8090/"
-//const val API_ENDPOINT = "https://api.lovemap.app/"
+//const val API_ENDPOINT = "http://192.168.0.58:8090/"
+const val API_ENDPOINT = "https://api.lovemap.app/"
 
 const val LINK_PREFIX_API_CALL = "https://api.lovemap.app/lover?uuid="
 const val LINK_PREFIX_VISIBLE = "https://api.lovemap.app/join-us/lover?uuid="
