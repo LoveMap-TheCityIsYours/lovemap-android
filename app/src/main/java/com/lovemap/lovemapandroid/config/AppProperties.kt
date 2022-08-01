11e77553e204d09e45e5f61d8a23c09983d409f9
Attila Palfi <attila.palfi.92@gmail.com>
2022-08-01 14:43:58 +0200

    changing config back to prod

--- app/src/main/java/com/lovemap/lovemapandroid/config/AppProperties.kt
+++ app/src/main/java/com/lovemap/lovemapandroid/config/AppProperties.kt
@@ -1,12 +1,7 @@
 package com.lovemap.lovemapandroid.config
 
-// home:
-const val API_ENDPOINT = "http://192.168.0.58:8090/"
-
-// reka home:
-//const val API_ENDPOINT = "http://192.168.0.143:8090/"
-
-// prod:
-//const val API_ENDPOINT = "https://api.lovemap.app/"
+//const val API_ENDPOINT = "http://192.168.0.143:8090/" // reka home
+//const val API_ENDPOINT = "http://192.168.0.58:8090/" // home
+const val API_ENDPOINT = "https://api.lovemap.app/" // prod
 
 
