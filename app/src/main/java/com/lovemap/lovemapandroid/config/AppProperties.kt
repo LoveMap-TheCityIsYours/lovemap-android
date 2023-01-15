235e10378a88ef0fd2b8ff32cc82ab52e37f0c76
Attila Palfi <attila.palfi.92@gmail.com>
2023-01-15 10:50:23 +0100

    - android studio and project dependencies upgraded
    - TODO: term of use must be re-accepted because show usernames to others, show alert dialog on end partnership
    - TODO: fix bugs:
    - after ended partnership, profile page "No partner yet" text still opens previous partner
    - after ended partnership view other activity still shows other user as partner

--- app/src/main/java/com/lovemap/lovemapandroid/config/AppProperties.kt
+++ app/src/main/java/com/lovemap/lovemapandroid/config/AppProperties.kt
@@ -1,5 +1,5 @@
 package com.lovemap.lovemapandroid.config
 
 //const val API_ENDPOINT = "http://192.168.0.143:8090/" // reka home
-const val API_ENDPOINT = "http://192.168.0.143:8090/" // home
-//const val API_ENDPOINT = "https://api.lovemap.app/" // prod
+//const val API_ENDPOINT = "http://192.168.0.143:8090/" // home
+const val API_ENDPOINT = "https://api.lovemap.app/" // prod
